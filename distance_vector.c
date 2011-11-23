95109a6516027415e98d03342aa79eb5d66c4079
mayur <mayur@mayur-ThinkPad-T420.(none)>
2011-11-23 18:48:37 -0500

    File read and data structures setup for DV

--- distance_vector.c
+++ distance_vector.c
@@ -1,3 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"distance_vector.h"
+
+node *nodelist;
+int total_nodes;
+FILE *file;
+
+void initialize_topology()
+{
+        int i,j;
+        fseek(file,0,SEEK_SET);
+        fscanf(file,"%d\n",&total_nodes);
+
+	printf("Total nodes %d\n",total_nodes);
+
+        nodelist = (node *)malloc(sizeof(node)*total_nodes);
+
+        for(i = 0;i< total_nodes;i++)
+	{ //initialize nodelist structure
+                nodelist[i].id = i;
+		nodelist[i].no_of_neighbors = 0;
+		nodelist[i].dv = (double*) malloc(sizeof(double)*total_nodes);
+		for(j=0;j<total_nodes;j++)
+		{
+			if(i==j)
+				nodelist[i].dv[j] = 0;
+			else
+				nodelist[i].dv[j] = INF;
+		}
+		nodelist[i].neighbor_list = (neighbor*)malloc(sizeof(neighbor)*total_nodes);
+		memset(nodelist[i].neighbor_list,0,sizeof(neighbor)*total_nodes);
+        }
+
+	printf("out of inital setup.. reading file now\n");
+        //memset(path,-1,total_nodes*sizeof(int)); //reset path array
+
+        int temp_i = -1,temp_j = -1;
+        double temp_cost = 0.0f;
+        while(fscanf(file,"%d %d %lf",&temp_i,&temp_j,&temp_cost)!= EOF){
+		nodelist[temp_i-1].neighbor_list[nodelist[temp_i-1].no_of_neighbors].id = temp_j-1;
+		nodelist[temp_j-1].neighbor_list[nodelist[temp_j-1].no_of_neighbors].id = temp_i-1;
+
+		nodelist[temp_i-1].neighbor_list[nodelist[temp_i-1].no_of_neighbors].dv = (double*)malloc(sizeof(double)*total_nodes);
+		for(i=0;i<total_nodes;i++)
+			nodelist[temp_i-1].neighbor_list[nodelist[temp_i-1].no_of_neighbors].dv[i] = INF;
+		nodelist[temp_j-1].neighbor_list[nodelist[temp_j-1].no_of_neighbors].dv = (double*)malloc(sizeof(double)*total_nodes);
+		for(i=0;i<total_nodes;i++)
+			nodelist[temp_j-1].neighbor_list[nodelist[temp_j-1].no_of_neighbors].dv[i] = INF;
+
+		nodelist[temp_i-1].no_of_neighbors++;
+		nodelist[temp_j-1].no_of_neighbors++;
+
+		nodelist[temp_i-1].dv[temp_j-1] = temp_cost;
+		nodelist[temp_j-1].dv[temp_i-1] = temp_cost;
+        }
+}
+
+void print_topology()
+{
+	int i,j;
+	for(i=0;i<total_nodes;i++)
+	{
+		printf("\nFor node %d\n",nodelist[i].id+1);
+		printf("No of neighbours %d\n",nodelist[i].no_of_neighbors);
+		printf("Neighbors are\n");
+		for(j=0;j<nodelist[i].no_of_neighbors;j++)
+			printf("%d\t",nodelist[i].neighbor_list[j].id+1);
+		printf("\nDistance vector is\n");
+		for(j=0;j<total_nodes;j++)
+			printf("%.2lf\t",nodelist[i].dv[j]);
+	}			
+}
+
 int main(int argc, char *argv[]){
+	total_nodes = -1;
+        if(argc != 5){
+                perror("Incorrect command line arguments\n");
+                exit(-1);
+        }
+        file = fopen(argv[2],"r");
+        if(!file){
+                perror("Cannot Open File\n");
+                exit(-1);
+        }
+	printf("File open\n");
+	initialize_topology();
+	print_topology();
 	return 0;
 }
--- distance_vector.h
+++ distance_vector.h
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#ifndef DISTANCE_VECTOR
+#define DISTANCE_VECTOR
+#include<fcntl.h>
+
+#define INF 99
+
+typedef struct neighbor_ {
+	int id;
+	double *dv;
+} neighbor;
+
+typedef struct node_ {
+	int id;
+	int no_of_neighbors;
+	neighbor *neighbor_list;
+	double *dv;
+} node;
+
+extern node *nodelist;
+extern int total_nodes;
+
+#endif
