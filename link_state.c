ae4c9b0995128b9842b99e0192636ede6c9c5f75
Sagar Natekar <sagar.natekar@gmail.com>
2011-11-24 16:37:44 -0500

    Done with Link state. Problen in finding CPU Time. Tasks remaining.

--- link_state.c
+++ link_state.c
@@ -14,10 +14,48 @@ double getcputime(void)
         return t; 
 }
 
+void save_path(int current_node){
+	int i;
+	int j = 0,k;
+	for(i=0;i<total_nodes;i++)
+	{
+		//printf("Pred for %d is: %d\n",i+1,node[i].pred+1);
+		if(i!=current_node - 1){
+			//node[current_node - 1].saved_cost
+			k = i;
+			j = node[i].pred;
+		
+			while(j!=current_node-1){
+				k = j;
+				j = node[j].pred;
+			}	
+			node[current_node - 1].next_hop[i] = k;
+			node[current_node - 1].saved_cost[i] = node[i].cost;	
+		}
+		else{
+			node[current_node - 1].next_hop[i] = i;
+			node[current_node - 1].saved_cost[i] = 0.0f;
+		}	
+	}
+	
+}
+
+void find_path(int node1, int node2)
+{
+}
+
+void reset_topology(){
+	int i;
+	for(i=0;i<total_nodes;i++){
+		node[i].flag = 0;
+		node[i].pred = i;
+	}
+	
+}
 int main(int argc, char *argv[]){
 	int i;
 	clock_t start,end;
-	double cpu_time_used;
+	long cpu_time_used;
 	struct tms *buf,*buf1;
 	total_nodes = -1;
 	
@@ -27,26 +65,44 @@ int main(int argc, char *argv[]){
 	}
 	file = fopen(argv[1],"r");
 	initialize_topology();
-//	print_topology();
-	//start = clock();
-	//struct tms buf;
-	start = times(&buf);
+	start = clock();
+	//start = times(buf);
+
 	//s = getcputime();
-	printf("Start : %.8lf\n",(double)start);
-	link_state(atoi(argv[2]),atoi(argv[3]),0);
+	printf("Start : %ld\n",(long)start);
+	for(i=0;i<total_nodes;i++)
+	{
+		link_state(i+1,0);
+		save_path(i+1);
+	//	print_topology(i+1);
+		reset_topology();
+	}
+
+	//end = clock();
+	end = times(&buf1);
+	printf("End : %ld\n",(long)end);
+	cpu_time_used = ((long) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time taken : %ld\n",cpu_time_used);
+
+	print_topology(atoi(argv[2]));
+	print_topology(atoi(argv[3]));
+	printf("Cost of the Least cost path between Node1 (%d) and Node2(%d) is : %lf\n",atoi(argv[2]),atoi(argv[3]),node[atoi(argv[2])-1].saved_cost[atoi(argv[3])-1]);
+	
 	for(i=0;i<total_nodes;i++)
 	{
 		free(node[i].edge_cost);
+		free(node[i].saved_cost);
+		free(node[i].next_hop);
 	}
 	free(node);
-	//struct tms buf1;
+	/*//struct tms buf1;
 	end = times(&buf1);
 	printf("End : %.8lf\n",(double)end);
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	initialize_topology();
 	link_state(atoi(argv[3]),atoi(argv[2]),1);
 	printf("Time taken : %lf\n",cpu_time_used);
-
+	*/
 }	
 
 void initialize_topology(){
@@ -63,10 +119,13 @@ void initialize_topology(){
 
 	for(i = 0;i< total_nodes;i++){ //initialize node structure
 		node[i].edge_cost = (double *)malloc(sizeof(double) * total_nodes);
-		memset(node[i].edge_cost,0.0,sizeof(double)*total_nodes);
+		node[i].next_hop = (int *)malloc(sizeof(int) * total_nodes);
+		node[i].saved_cost = (double *)malloc(sizeof(double) * total_nodes);
+		for(j = 0;j < total_nodes;j++)
+			node[i].edge_cost[j] = INF;	
 		node[i].pred = i;
 		node[i].flag=0; //not yet permanent
-		node[i].cost=INF;//start all nodes with infinite cost
+	//	node[i].cost=INF;//start all nodes with infinite cost
 	}
 	
 	//memset(path,-1,total_nodes*sizeof(int)); //reset path array
@@ -79,9 +138,36 @@ void initialize_topology(){
 	}
 }
 
-void print_topology()
+
+void initialization()
 {
-	int i,j;
+	int k = 0;
+	int i;	
+	for(i=0;i<total_nodes;i++)
+        {
+                if(i!=k)
+                {
+                        if(node[k].edge_cost[i]!=INF){
+                                //printf("D[%d] = C(%d,%d) = %lf\n",i+1,k+1,i+1,node[k].edge_cost[i]);
+                                node[i].cost = node[k].edge_cost[i]; //initialize D(v) = c(u,v)
+                        }
+                        else{
+                                //printf("D[%d] = INFININTY\n",i+1);
+                                node[i].cost = INF;
+                        }
+                }
+                else{
+                        node[i].cost = 0.0f;
+                        //printf("D[%d] = 0\n",i+1);
+                }
+        }
+
+
+}
+void print_topology(int src)
+{
+	int k;
+	/*
 	for(i = 0;i < total_nodes; i++){
 		printf("%d : ",i+1);
 		for(j = 0;j < total_nodes; j++){
@@ -90,33 +176,64 @@ void print_topology()
 		}
 		printf("\n");
 	}
-	
+	*/
+	printf("\nRouting table of source node %d -->\n\n",src);
+        printf("  Destination\t  Next Hop \tCost to Destination\n");
+        printf("---------------------------------------------------\n");
+
+        for(k = 0;k < total_nodes; k++){
+                       // if(node[i].edge_cost[k]!=0)
+                        //if(node[k].cost!=999999)
+                        printf("  %d\t\t  %d   \t\t\t   %.2lf\n",k+1,node[src-1].next_hop[k]+1,node[src-1].saved_cost[k]);
+                        //else
+                        //printf("  %d\t  %d\tINFINITY\n",src,k+1);
+        }
+
 }
 
-void link_state(int src, int dest, char flag){
+int all_flags_set()
+{
+	int i;
+	for(i=0;i<total_nodes;i++)
+	{	
+		if(node[i].flag==0)
+			return 0;
+	}
+	return 1;
+}
+
+void link_state(int src, char flag){
 	
 	int i,k;
 	double min;
 
 	k=src-1;
-        node[src-1].cost=0.0f;
-        node[src-1].flag=1;
+        //node[src-1].cost=0.0f;
+        node[k].flag=1;
+
+	for(i=0;i<total_nodes;i++)
+	{
+		if(i!=k)
+		{
+			if(node[k].edge_cost[i]!=INF){
+				//printf("D[%d] = C(%d,%d) = %lf\n",i+1,k+1,i+1,node[k].edge_cost[i]);
+				node[i].cost = node[k].edge_cost[i]; //initialize D(v) = c(u,v)
+				node[i].pred = k;
+			}
+			else{
+				//printf("D[%d] = INFININTY\n",i+1);
+				node[i].cost = INF;
+			}
+		}
+		else{
+			node[i].cost = 0.0f;
+			//printf("D[%d] = 0\n",i+1);
+		}
+	}
 
         do
         {
-                for(i=0;i<total_nodes;i++)
-                {
-                        if(node[k].edge_cost[i]!=0 && node[i].flag == 0)
-                        {
-                                if(node[k].cost + node[k].edge_cost[i] < node[i].cost)  //update predecessor and cost to src node
-                                {
-                                        node[i].pred = k;
-                                        node[i].cost = node[k].cost+node[k].edge_cost[i];
-                                }
-                        }
-
-                }
-                k=0,min=INF;
+	        min=INF;
                 //Find next min
                 for(i=0;i<total_nodes;i++)
                 {
@@ -127,33 +244,23 @@ void link_state(int src, int dest, char flag){
                         }
                 }
                 node[k].flag=1;
-        }while(k!=(dest-1));
-	
-	if(!flag)
-	printf("\nLeast cost from source to destination is : %.2lf\n",node[dest-1].cost);
+                for(i=0;i<total_nodes;i++)
+                {
+		
+                        if(node[k].edge_cost[i]!=INF && node[i].flag == 0)
+                        {
+                                if(node[k].cost + node[k].edge_cost[i] < node[i].cost)  //update predecessor and cost to src node
+				{
+					//printf("Setting Pred for %d to %d\n", i+1,k+1);
+                                        node[i].pred = k;
+                                        node[i].cost = node[k].cost+node[k].edge_cost[i];
+                                }
+                        }
+			
 
-	printf("\nRouting table of source node %d -->\n\n",src);
-	printf("  i\t  j\tCost\n");
-	printf("------\t------\t----\n");
+                }
+
+        }while(!all_flags_set());
 	
-        for(k = 0;k < total_nodes; k++){
-                       // if(node[i].edge_cost[k]!=0)
-			if(node[k].cost!=999999)
-                        printf("  %d\t  %d\t%.2lf\n",src,k+1,node[k].cost);
-			else
-			printf("  %d\t  %d\tINFINITY\n",src,k+1);
-        }
-	//printf("--------------------\n");
-	/*
-	printf("\nRouting table of destination node %d -->\n\n",dest);
-        printf("Node 1\tNode 2\tCost\n");
-        printf("------\t------\t----\n");
-        
-        for(k = 0;k < total_nodes; k++){
-                       // if(node[i].edge_cost[k]!=0)
-                        printf("  %d\t  %d\t%.2lf\n",dest,k+1,node[k].cost);
-        }
-	*/
-	printf("\n\n");
 }
 
--- link_state.h
+++ link_state.h
@@ -8,12 +8,15 @@
 #include<time.h>
 #include<sys/time.h>
 #include<sys/resource.h>
-#define INF 999999
+#define INF 999999.0
 typedef struct node_{
 	double *edge_cost;
 	int pred;
 	int flag; //denotes whether node's distance to src is permanent or can change
 	double cost; // length of node to source
+	
+	double *saved_cost;
+	int *next_hop;
 }Node;
 
 Node *node;
@@ -21,5 +24,6 @@ FILE *file;
 int total_nodes;
 
 void initialize_topology();
-void print_topology();
-void link_state(int, int, char);
+void print_topology(int);
+void link_state(int, char);
+void initialization();
